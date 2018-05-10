50a6add0553465cf045db5914bcad1d82f074d69
Daniele Potertì <daniele.poterti@gmail.com>
2018-05-10 13:50:54 +0200

    added crypto js dependencies and SHA256 hash function

--- block.js
+++ block.js
@@ -1,3 +1,5 @@
+const SHA256 = require('crypto-js/sha256');
+
 class Block {
     constructor(timestamp, lastHash, hash, data) {
         this.timestamp = timestamp;
@@ -5,6 +7,7 @@ class Block {
       this.hash = hash;
       this.data = data;
     }
+
     toString() {
 
         return `Block -
@@ -21,11 +24,15 @@ class Block {
         static mineBlock(lastBlock, data){
             const timestamp = Date.now();
             const lastHash = lastBlock.hash;
-            const hash = 'todo-hash';
+            const hash = Block.hash(timestamp,lastBlock,data);
 
             return new this(timestamp, lastHash, hash, data);
         }
 
+
+        static hash(timestamp, lastHash, data) {
+            return SHA256(`${timestamp}${lastHash}${data}`).toString();
+        }
     }
 
     module.exports = Block;
