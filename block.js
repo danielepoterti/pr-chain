f4f3369ef3fdc5bbdfb47545ba32c0424e75ed23
Daniele Potertì <daniele.poterti@gmail.com>
2018-06-06 16:12:47 +0200

    added isValidChain to Blockchain class

--- block.js
+++ block.js
@@ -21,17 +21,23 @@ class Block {
           return new this('Genesis time', '--------', 'f1r57-h45h', []);
         }
 
-        static mineBlock(lastBlock, data){
+        static mineBlock(lastBlock, data) {
             const timestamp = Date.now();
             const lastHash = lastBlock.hash;
             const hash = Block.hash(timestamp,lastBlock,data);
 
             return new this(timestamp, lastHash, hash, data);
         }
-
+ 
 
         static hash(timestamp, lastHash, data) {
             return SHA256(`${timestamp}${lastHash}${data}`).toString();
+        }
+
+        static blockHash(block){
+            const { timestamp, lastHash, data} = block;
+
+            return Block.hash(timestamp, lastHash, data);
         }
     }
 
