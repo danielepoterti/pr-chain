45aab1733ec7ba2bfd0c626c03e867588caa7fdc
Daniele Potertì <daniele.poterti@gmail.com>
2018-06-06 18:27:07 +0200

    organising the project folder

--- blockchain/index.test.js
+++ blockchain/index.test.js
@@ -1,4 +1,4 @@
-const Blockchain = require('./blockchain');
+const Blockchain = require('./index');
 const Block = require('./block');
 
 describe('Blockchain', () => {
@@ -36,4 +36,17 @@ describe('Blockchain', () => {
       bc2.chain[1].data = 'Not foo';
       expect(bc.isValidChain(bc2.chain)).toBe(false);
     });
+
+    it('replaces the chain with a valid chain', () => {
+        bc2.addBlock('goo'); //bc2 è più lungo e deve essere sostituito
+      bc.replaceChain(bc2.chain);
+      expect(bc.chain).toEqual(bc2.chain);
+    });
+    
+    it('does not replace the chain with one of less than or equal length', () => {
+        bc.addBlock('foo'); //bc2 è più piccolo e non deve essere sostituito
+      bc.replaceChain(bc2.chain);
+      expect(bc.chain).not.toEqual(bc2.chain);
+    });
+    
 }); 
