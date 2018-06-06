a74817dfce98d859cd14548112464050cb550959
Daniele Potertì <daniele.poterti@gmail.com>
2018-06-06 17:50:00 +0200

    added replaceChain

--- blockchain.js
+++ blockchain.js
@@ -33,6 +33,24 @@ class Blockchain {
 		}
 		return true;
     }
+
+    //se un altro nodo invia una catena valida
+    //viene sostituita la catena dell'host con la nuova ricevuta
+    
+    replaceChain(newChain) {
+        if (newChain.length <= this.chain.length) {
+          console.log('Received chain is not longer than the current chain.'); //non è più lunga
+          return;
+        } else if (!this.isValidChain(newChain)) {
+          console.log('The received chain is not valid.'); //non è valida
+          return;
+        }
+      
+        //se i due controlli sono successful allora può essere sostituita
+
+        console.log('Replacing blockchain with the new chain.');
+        this.chain = newChain;
+      }
 }
 
 module.exports = Blockchain;
